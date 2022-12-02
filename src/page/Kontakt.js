baa0b9230531316fad6e3f29be7d3fbad0452c1e
Enebye <Michelle-Chantal@hotmail.com>
2022-12-02 11:13:50 +0100

    Kontakt page

---
+++ src/page/Kontakt.js
@@ -0,0 +1,18 @@
+//Michelle 
+
+import React from "react"; 
+import Overskrift from '../components/Overskrift';
+export default function() {
+
+    return(
+        <>
+        <section>
+         <div>
+         <Overskrift overskrift="Kontakt salonen" />
+
+        </div>
+
+        </section>
+        </>
+    )
+} 
