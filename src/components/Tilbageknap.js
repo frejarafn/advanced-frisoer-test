4cf96f285d2ea50e10bd6174109eb12327a00988
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-12-02 10:28:22 +0100

    tilbageknap

--- src/components/Tilbageknap.js
+++ src/components/Tilbageknap.js
@@ -1,1 +1,17 @@
 //freja
+
+import { useNavigate } from "react-router-dom";
+import '../styles/Tilbageknap.css';
+import { IoIosArrowBack } from 'react-icons/io';
+
+// Her ses en onClick funktion, hvor navigate bruges til at navigere 1 side tilbage når man trykker på billedet
+export const Tilbageknap = () => {
+    let navigate = useNavigate();
+    return (
+        <>
+            <button className="backbutton" onClick={() => navigate(-1)}>
+                <IoIosArrowBack className="headerback"></IoIosArrowBack>
+            </button>
+        </>
+    );
+};
