1f175faae195273bca5d3cb495d37f2748d08dc5
Nadia-Anjum <diaanjum@outlook.dk>
2022-12-02 10:56:17 +0100

    tilføjet tekstindhold

--- src/page/Profil.js
+++ src/page/Profil.js
@@ -4,6 +4,9 @@ export default function Profil() {
     return (
             <section>
                     <div>
+                        <p className="profil-tekst">Navn</p>
+                    </div>
+                    <div>
                         <h3>Mine tider</h3>
                         <h5>Dame klipning - d. 15 dec. kl. 10.00</h5>
                     </div>
@@ -12,10 +15,20 @@ export default function Profil() {
                         <h3>Gemte betalingskort</h3>
                         <h5>xxxx-xxxx-xxxx-xxxx</h5>
                     </div>
+
                     <button className="betalingskort-knap">
                         <p className="betalingskort-tekst-knap">Tilføj betalingskort</p>
                     </button>
-                    
+
+
+                    <button>
+                        indstilling
+                    </button>
+
+                    <button>
+                        log ud
+                    </button>
+
             </section>
 
     )
