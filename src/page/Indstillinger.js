10953c375b000694a92ddef94590dbe3f0aaaabc
Nadia-Anjum <diaanjum@outlook.dk>
2022-12-02 10:46:02 +0100

    tilføjet tekstindhold på siderne

--- src/page/Indstillinger.js
+++ src/page/Indstillinger.js
@@ -1,1 +1,12 @@
 // Nadia
+
+export default function Indstillinger() {
+    return (
+            <section>
+                    <div>
+                    <h1>Brugerindhold</h1>
+                    </div>
+            </section>
+
+    )
+}
