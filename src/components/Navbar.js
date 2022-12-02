b8bd4617be1015b7d33f161bed1d43fbae3df833
Gerda522 <gerdusyte522@gmail.com>
2022-12-02 11:00:18 +0100

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,25 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <NavLink className="forside" to="/"></NavLink>
+      <NavLink className="forside" to="/">
+        Forside
+      </NavLink>
 
-      <NavLink className="behandlinger" to="/Behandlinger"></NavLink>
+      <NavLink className="behandlinger" to="/Behandlinger">
+        Behandlinger
+      </NavLink>
 
-      <NavLink className="booking" to="/Booking"></NavLink>
+      <NavLink className="booking" to="/Booking">
+        Booking
+      </NavLink>
 
-      <NavLink className="login" to="/Login"></NavLink>
+      <NavLink className="profil" to="/Profil">
+        Profil
+      </NavLink>
 
-      <NavLink className="profil" to="/Profil"></NavLink>
-
-      <NavLink className="information" to="/Information"></NavLink>
+      <NavLink className="information" to="/Information">
+        Information
+      </NavLink>
     </nav>
   );
 }
