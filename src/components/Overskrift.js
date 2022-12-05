2524d97a76eeabec53b9c37e7ce1b8def1915e07
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-12-05 09:21:26 +0100

    header og overskrift

--- src/components/Overskrift.js
+++ src/components/Overskrift.js
@@ -1,13 +1,13 @@
 //Michelle
 
-import React from "react"; 
-import '../styles/.css';
+import React from "react";
+import '../styles/Overskrift.css';
 
 export default function overskrift(props) {
 
-    return(
+    return (
         <>
-         <h1>{props.name}</h1>
+            <h1>{props.name}</h1>
         </>
     )
 }
