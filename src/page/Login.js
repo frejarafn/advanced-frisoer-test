d0d1cb701f5c512b045ccea5611cc1d6b20c2712
Gerda522 <gerdusyte522@gmail.com>
2022-12-05 12:03:28 +0100

    Opret start

--- src/page/Login.js
+++ src/page/Login.js
@@ -3,11 +3,11 @@
 import React from "react";
 
 export default function Login() {
-    return (
-        <div>
-            <main>
-                <h1>Forside</h1>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <main>
+        <h1>Login</h1>
+      </main>
+    </div>
+  );
 }
