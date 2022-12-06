5d256ec39cfe486b7d859025be6e741e23c57c51
Gerda522 <gerdusyte522@gmail.com>
2022-12-06 14:42:40 +0100

    Login og firebase

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,13 +1,48 @@
 // Gerda
-
+import "../styles/Login.css";
 import React from "react";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  let navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    signInWithEmailAndPassword(auth, email, password);
+
+    navigate("/");
+  };
   return (
-    <div>
-      <main>
-        <h1>Login</h1>
-      </main>
+    <div className="login">
+      <h1>Login</h1>
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-control"
+          placeholder="E-mail"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Kodeord"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+      <button className="opret-btn" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
--- src/page/Profil.js
+++ src/page/Profil.js
@@ -2,57 +2,71 @@
 
 import "../styles/Profil.css";
 import Header from "../components/Header";
-import { BsPersonFill } from 'react-icons/bs';
-import { TiDeleteOutline } from 'react-icons/ti';
+import { BsPersonFill } from "react-icons/bs";
+import { TiDeleteOutline } from "react-icons/ti";
 import { NavLink } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebaseConfig";
+import { signOut } from "firebase/auth";
 
 export default function Profil() {
-    return (
-            <section>
-                <Header></Header>
-                    {/*Profil billede og tekst*/}
-                    <div className="profil-main">
-                        <BsPersonFill className="profil-billede"/>
-                    </div>
+  const [user] = useAuthState(auth);
+  return (
+    <section>
+      <Header></Header>
+      {/*Profil billede og tekst*/}
+      <div className="profil-main">
+        <BsPersonFill className="profil-billede" />
+      </div>
 
-                    <div className="profil-tekst">
-                        <p className="profil-navn">Freja</p>
-                    </div>
+      <div className="profil-tekst">
+        <p className="profil-navn">
+          {" "}
+          {user && (
+            <>
+              <span className="user">{user.displayName || user.email}</span>
+            </>
+          )}
+        </p>
+      </div>
 
-                     {/*Main indhold*/}
-                    <div className="profil-tider">
-                        <h2 className="tiderOverskrift">Mine tider</h2>
-                        <h4 className="tidstekst">Dame klipning - d. 15 dec. kl. 10.00 <TiDeleteOutline className="slet-ikon1"/></h4>
-                    </div>
+      {/*Main indhold*/}
+      <div className="profil-tider">
+        <h2 className="tiderOverskrift">Mine tider</h2>
+        <h4 className="tidstekst">
+          Dame klipning - d. 15 dec. kl. 10.00{" "}
+          <TiDeleteOutline className="slet-ikon1" />
+        </h4>
+      </div>
 
-                    <div className="profil-betaling">
-                        <h2 className="betalingoverskrift">Gemte betalingskort</h2>
-                        <h4 className="betalingstekst">xxxx-xxxx-xxxx-xxxx <TiDeleteOutline className="slet-ikon2"/></h4>
-                    </div>
+      <div className="profil-betaling">
+        <h2 className="betalingoverskrift">Gemte betalingskort</h2>
+        <h4 className="betalingstekst">
+          xxxx-xxxx-xxxx-xxxx <TiDeleteOutline className="slet-ikon2" />
+        </h4>
+      </div>
 
-                    <button className="betalingskort-knap">
-                        Tilføj betalingskort
-                    </button>
+      <button className="betalingskort-knap">Tilføj betalingskort</button>
 
-                        <br></br>
-                        <br></br>
+      <br></br>
+      <br></br>
 
-                     {/*Indstillinger knap*/}
-                    <div className="indstillinger-knap">
-                        <NavLink to ="/Indstillinger">
-                            Indstillinger
-                        </NavLink>
-                    </div>
+      {/*Indstillinger knap*/}
+      <div className="indstillinger-knap">
+        <NavLink to="/Indstillinger">Indstillinger</NavLink>
+      </div>
 
-                        <br></br>
+      <br></br>
 
-                     {/*Log ud knap*/}
-                    <div className="logud-knap">
-                        <NavLink to ="/Login">Log ud</NavLink>
-                    </div>
-                    
-
-            </section>
-
-    )
+      {/*Log ud knap*/}
+      <div
+        className="logud-knap"
+        onClick={() => {
+          signOut(auth);
+        }}
+      >
+        <NavLink to="/Login">Log ud</NavLink>
+      </div>
+    </section>
+  );
 }
