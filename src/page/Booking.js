2ce61a158de26c2e02f72e371b6eafe77a5a2f7d
Enebye <Michelle-Chantal@hotmail.com>
2022-12-08 13:00:21 +0100

    Michelle

--- src/page/Booking.js
+++ src/page/Booking.js
@@ -3,8 +3,8 @@ import React from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 import "../styles/Booking.css";
 
+
 const Booking = () => {
-  
   const [open, setOpen] = React.useState(false); 
 
   const handleOpen = () => {
@@ -20,22 +20,23 @@ const Booking = () => {
   }; 
 
   return (
-    
+  
     <div className="dropdown">
-      <button onClick={handleOpen}>Dropdown</button>
-      {open ? (
-        <ul className="menu">
-          <li className="menu_item">
-            <button onClick={handlemenuOne}>menu 1</button>
-          </li>
-          <li className="menu_item">
-            <button onClick={handlemenuTwo}>Menu 2</button>
-          </li>
-        </ul>
-      ) : null}
-      {open ? <div>Is Open</div> : <div>Is Closed</div>}
-    </div>
-  ); 
+       
+    <button onClick={handleOpen}>Dropdown</button>
+    {open ? (
+      <ul className="menu">
+        <li className="menu_item">
+          <button onClick={handlemenuOne}>menu 1</button>
+        </li>
+        <li className="menu_item">
+          <button onClick={handlemenuTwo}>Menu 2</button>
+        </li>
+      </ul>
+    ) : null}
+    {open ? <div>Is Open</div> : <div>Is Closed</div>}
+  </div>
+); 
 }; 
 
 export default Booking; 
