9892d091d3a7c62a6e32edbfb89d544b55473a5b
Gerda522 <gerdusyte522@gmail.com>
2022-12-09 10:13:23 +0100

    Aabningstider

--- src/page/Aabningstider.js
+++ src/page/Aabningstider.js
@@ -3,6 +3,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import "../styles/Aabningstider.css";
 
 export default function Aabningstider() {
   return (
@@ -12,7 +13,28 @@ export default function Aabningstider() {
         <Header />
       </div>
 
-      <div className="aabningstider"></div>
+      <div className="aabningstider">
+        <div className="Dage">
+          <p>Mandag </p>
+          <p>Tirsdag </p>
+          <p>Tirsdag </p>
+          <p>Onsdag </p>
+          <p>Torsdag </p>
+          <p>Fredag </p>
+          <p>Lørdag </p>
+          <p>Søndag </p>
+        </div>
+        <div className="Tidspunkter-lige">
+          <p>09:00 - 17:00</p>
+          <p>Lukket </p>
+          <p>Tirsdag </p>
+          <p>09:00 - 17:00 </p>
+          <p>09:00 - 17:00</p>
+          <p>09:00 - 17:00 </p>
+          <p>09:00 - 14:00 </p>
+          <p>Lukket </p>
+        </div>
+      </div>
     </div>
   );
 }
