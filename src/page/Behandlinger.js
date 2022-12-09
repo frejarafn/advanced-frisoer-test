aeed37e6f2b0a50d825f6f7f4f59a2daa34fd024
Gerda522 <gerdusyte522@gmail.com>
2022-12-09 09:58:43 +0100

    Header

--- src/page/Behandlinger.js
+++ src/page/Behandlinger.js
@@ -1,6 +1,7 @@
 // Freja
 import React from "react";
 import BehandlingSection from "../components/behandlingfetch/BehandlingSection";
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
 export default function Behandlinger() {
