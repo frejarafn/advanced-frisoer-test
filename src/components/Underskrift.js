4646fcc00308913b9d315b4adc74b5a02525f128
Nadia-Anjum <diaanjum@outlook.dk>
2022-12-09 13:43:14 +0100

    kommentar tilføjet

--- src/components/Underskrift.js
+++ src/components/Underskrift.js
@@ -1,8 +1,12 @@
 // Michelle 
+
+ // Dette er en fil for ændringer af h2 tags på siderne, som kan bruges
+
 import React from "react";
 import '../styles/Underskrift.css';
 
 export default function Underskrift(props) {
+
 
     return (
         <>
