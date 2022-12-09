76262accd5f4e21739b472879519c4cd2d575c1f
Enebye <Michelle-Chantal@hotmail.com>
2022-12-09 12:04:12 +0100

    Underskrift component

---
+++ src/components/Underskrift.js
@@ -0,0 +1,14 @@
+// Michelle 
+import React from "react";
+import '../styles/Underskrift.css';
+
+export default function Underskrift(props) {
+
+    return (
+        <>
+        <div>
+            <h2>{props.underskrift}</h2>
+        </div>
+        </>
+    ); 
+};  
--- src/page/Forside.js
+++ src/page/Forside.js
@@ -1,43 +1,30 @@
 // Michelle
 import React from "react";
 import "../styles/Forside.css";
-import Header from '../components/HeaderForside';
-import Overskrift from '../components/Overskrift';
+import HeaderForside from '../components/HeaderForside';
+import Underskrift from '../components/Underskrift';
 import Karussel from '../components/Karussel';
 import Sortiment from '../components/Sortiment';
 
 export default function Forside() {
 
-  const Text= ['Alle pensionister og studerende får 20%',
-                 'på klipning og 10% på farvebehandling',
-                ]
- 
-  return (
-   <section className="Container">
-    <Header/>
-    <h2>Velkommen Freja</h2>
-    <div className="Favoritter">
-    <Overskrift overskrift="Dine Favoritter" />
-    </div>
-    <div className="Favorit_container">
-      <Sortiment/>
-    
-    </div>
-    <div className="tilbud_container">
-      <Overskrift overskrift="Tilbud og kampagner" />
-      <Karussel />
-      </div>
-    <div className="skrift_container">
-    {Text.map (tekst=> <p>{tekst}</p>)} 
-    </div>
-     
-      <div>
-     
-      </div>
-      <div className="options">
-       <Sortiment/>
-      </div>
 
-   </section>
+  return( 
+  <section>
+    <HeaderForside /> 
+    <div> 
+     
+    </div> 
+    <div className="Favoritter">
+    <Underskrift underskrift="Favoritter" />
+    </div>
+
+
+
+
+
+
+  </section>
+
   );
-};
+}; 
