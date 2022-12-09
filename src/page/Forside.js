419f53b5c40a66672341435f4a748ec055debe25
Enebye <Michelle-Chantal@hotmail.com>
2022-12-09 11:42:06 +0100

    Michelle

--- src/page/Forside.js
+++ src/page/Forside.js
@@ -1,56 +1,43 @@
 // Michelle
 import React from "react";
 import "../styles/Forside.css";
-import Header from "../components/HeaderForside";
-import Overskrift from "../components/Overskrift";
-import Karussel from "../components/Karussel";
-import Sortiment from "../components/Sortiment";
-import Navbar from "../components/Navbar";
+import Header from '../components/HeaderForside';
+import Overskrift from '../components/Overskrift';
+import Karussel from '../components/Karussel';
+import Sortiment from '../components/Sortiment';
 
 export default function Forside() {
-  const Text = [
-    "Alle pensionister og studerende får 20%",
-    "på klipning og 10% på farvebehandling",
-  ];
 
+  const Text= ['Alle pensionister og studerende får 20%',
+                 'på klipning og 10% på farvebehandling',
+                ]
+ 
   return (
    <section className="Container">
-    <Header />
+    <Header/>
     <h2>Velkommen Freja</h2>
     <div className="Favoritter">
-    <h2>Dine Favoritter</h2> 
+    <Overskrift overskrift="Dine Favoritter" />
     </div>
     <div className="Favorit_container">
       <Sortiment/>
     
     </div>
     <div className="tilbud_container">
-      <h2>Tilbud og kampagner</h2> 
+      <Overskrift overskrift="Tilbud og kampagner" />
       <Karussel />
+      </div>
+    <div className="skrift_container">
+    {Text.map (tekst=> <p>{tekst}</p>)} 
     </div>
-      <Navbar />
-      <Header />
-      <h2>Velkommen Freja</h2>
-      <div className="Favoritter">
-        <Overskrift overskrift="Dine Favoritter" />
+     
+      <div>
+     
       </div>
-      <div className="Favorit_container">
-        <Sortiment />
-      </div>
-      <div className="tilbud_container">
-        <Overskrift overskrift="Tilbud og kampagner" />
-        <Karussel />
-      </div>
-      <div className="skrift_container">
-        {Text.map((tekst) => (
-          <p>{tekst}</p>
-        ))}
+      <div className="options">
+       <Sortiment/>
       </div>
 
-      <div></div>
-      <div className="options">
-        <Sortiment />
-      </div>
-    </section>
+   </section>
   );
-}
+};
