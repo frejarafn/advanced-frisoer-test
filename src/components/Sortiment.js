c9f941d62e79086d4ad61bfb1428c72994226e7e
Enebye <Michelle-Chantal@hotmail.com>
2022-12-13 15:54:24 +0100

    sortiment

--- src/components/Sortiment.js
+++ src/components/Sortiment.js
@@ -5,18 +5,12 @@ import "../styles/Sortiment.css";
 
 export default function sortiment() {
     
-  const image = [1, 2, 3, 4, 5].map((n) => `/images/KigInd${n}.jpg`);
+ 
 
   return (
     <>
       <div className="Container_box">
-        {image.map((image, key) => {
-          return (
-            <div key={key} className="item">
-              <img src={image} alt="" />
-            </div>
-          );
-        })}
+        
       </div>
     </>
   );
