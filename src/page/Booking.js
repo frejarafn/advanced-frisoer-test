d03f538adf4c4c339c445ca416d9c17a689b60f8
Gerda522 <gerdusyte522@gmail.com>
2022-12-13 14:15:42 +0100

    Booking kommentering

--- src/page/Booking.js
+++ src/page/Booking.js
@@ -4,12 +4,12 @@ import "../styles/Booking.css";
 import Header from "../components/headers/HeaderBooking.js";
 import Dropdown from "../components/Dropdown";
 import Navbar from "../components/Navbar";
+// Kalender komponent importeres
 import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
+// Popup komponent importeres
 import Popup from "reactjs-popup";
-
 // Registrering af Syncfusion license key
 import { registerLicense } from "@syncfusion/ej2-base";
-
 import { IoMdArrowDropdown } from "react-icons/io";
 
 const Booking = () => {
@@ -38,12 +38,14 @@ const Booking = () => {
       <Header />
       <Navbar />
       <Dropdown />
+      {/*Kalender komponent bruges her*/}
       <div className="kalender">
         <DateTimePickerComponent placeholder="Vælg dato og tid"></DateTimePickerComponent>
       </div>
       <div className="kommentarer-form">
         <label htmlFor="">Yderligere kommentarer? </label>
         <input type="Kommentarer" className="form-control" />
+        {/*Popup komponent bruges her*/}
         <Popup
           trigger={<button className="book-btn"> Book </button>}
           modal
@@ -52,11 +54,9 @@ const Booking = () => {
           {(close) => (
             <div className="popup">
               <div className="popup-header">
-                {" "}
-                <h4>Din tid er bekræftet!</h4>{" "}
+                <h4>Din tid er bekræftet!</h4>
               </div>
               <div className="popup-content">
-                {" "}
                 Du har booket tid til: den XXX XXX XXX <br></br> Vi glæder os
                 til at se dig i salonen!
                 <button
