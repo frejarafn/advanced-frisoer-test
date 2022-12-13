c9068366fe84be35a35dcdb838fa89b637e96088
Enebye <Michelle-Chantal@hotmail.com>
2022-12-13 15:19:13 +0100

    Sortiment

--- src/components/Sortiment.js
+++ src/components/Sortiment.js
@@ -1,17 +1,23 @@
-import React from "react"; 
-import OptionsSortiment from '../components/OptionsSortiment';
+// Michelle
+
+import React from "react";
 import "../styles/Sortiment.css";
 
 export default function sortiment() {
+    
+  const image = [1, 2, 3, 4, 5].map((n) => `/images/KigInd${n}.jpg`);
 
-    return(
-        <>
-        <div className="Container_box">
-            <OptionsSortiment /> 
-           
-        </div>
-        </>
-    );
-}; 
-
-
+  return (
+    <>
+      <div className="Container_box">
+        {image.map((image, key) => {
+          return (
+            <div key={key} className="item">
+              <img src={image} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
